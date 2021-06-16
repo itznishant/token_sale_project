bf635565e80f2e995f297a92e5344e3417412084
Nishant <nishant@Nishants-MacBook-Air.local>
2021-06-16 23:08:32 +0530

    2. Transfer Tokens

--- contracts/SASIToken.sol
+++ contracts/SASIToken.sol
@@ -1,13 +1,39 @@
 pragma solidity ^0.5.16;
 
 contract SASIToken {
-	// Constructor
-	// Set  total # of tokens
-	// Read total # of tokens
+	string  public name   = "SASI Token"; // Token name
+	string  public symbol = "SASI"; // Symbol
+	string  public standard = "SASI Token v1.0"; // Declare totalSupply variable
 	uint256 public totalSupply; // Declare totalSupply variable
 
-	constructor () public {   // Token constructor
-		totalSupply = 1000000;
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+		);
+
+	mapping(address => uint256) public balanceOf;	
+
+	constructor (uint256 _initialSupply) public {   // Token constructor
+		balanceOf[msg.sender] = _initialSupply;		// allocate initial supply
+		totalSupply = _initialSupply;
+
+	}
+
+	// Transfer function 
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+	// Exception incase of insufficient balance
+	require(balanceOf[msg.sender] >= _value);
+
+	// Transfer the balance
+	balanceOf[msg.sender] -= _value;
+	balanceOf[_to] += _value;
+
+	// Transfer Event
+	emit Transfer(msg.sender, _to, _value);
+	
+	// Return a Boolean
+	return true;
 
 	}
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var SASIToken = artifacts.require("./SASIToken.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(SASIToken);
+  deployer.deploy(SASIToken, 1000000);
 };
